e020613ad48bdf4d8baecea1e49dff5e51f95437
gabymendoz <71575352+gabymendoz@users.noreply.github.com>
2020-11-28 17:11:07 -0500

    Add files via upload

---
+++ deberweb/form.js
@@ -0,0 +1,17 @@
+
+
+
+var palabra = prompt("¿Cual es su nombre? ");
+var frase =("Binevindo " + palabra +  " proceda a llenar el formulario  ");
+alert(frase);
+
+/*const nombre = document.getElementsById("nombre");
+const apellido = document.getElementsById('apellido');
+const edad = document.getElementsById('edad');
+const identida = document.getElementsById('identidad');
+const numero= document.getElementsById('celular');
+const email = document.getElementsById('correro');
+const cidudad = document.getElementsById('ciudad');
+const fecha = document.getElementsById('fecha');
+const estado = document.getElementsById('estado ');
+const estudio = document.getElementsById('estudio ');*/
